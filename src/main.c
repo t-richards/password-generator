52d81f7e0a907ef6c41aef9b339db10bcb48665f
Tom Richards <tom@tomrichards.net>
2018-10-05 23:23:52 -0400

    Remove gotos

--- src/main.c
+++ src/main.c
@@ -23,54 +23,16 @@ static void show_usage(int argc, char *argv[]) {
   printf("Usage:  %s [password_length] [num_passwords]\n", argv[0]);
 }
 
-int main(int argc, char *argv[]) {
-  char *password = NULL;
-  int result = -1, retval = 0;
-  int password_length = 64;
-  int num_passwords = 1;
-
-  /* Check if -h or --help was specified */
-  if (should_show_usage(argc, argv)) {
-    show_usage(argc, argv);
-    goto cleanup;
-  }
-
-  /* Parse password length argument, if provided */
-  if (argc >= 2) {
-    password_length = atoi(argv[1]);
-    if (password_length <= 0) {
-      fprintf(stderr, "Password length must be greater than zero.\n");
-      retval = 1;
-      goto cleanup;
-    }
-  }
-
-  /* Parse number of passwords argument, if provided */
-  if (argc >= 3) {
-    num_passwords = atoi(argv[2]);
-    if (num_passwords <= 0) {
-      fprintf(stderr,
-              "Number of passwords to generate must be greater than zero.\n");
-      retval = 1;
-      goto cleanup;
-    }
-  }
-
-  /* Allocate memory */
-  password = calloc(1, password_length);
-  if (password == NULL) {
-    fprintf(stderr, "Failed to allocate memory: %s\n", strerror(errno));
-    retval = 1;
-    goto cleanup;
-  }
+static int do_password_generation(char *password, int password_length,
+                                  int num_passwords) {
+  int result = -1;
 
   /* Lock the heap-allocated buffer into memory */
   /* This prevents the memory page from swapping to disk */
   result = mlock(password, password_length);
   if (result != 0) {
     fprintf(stderr, "Failed to lock process memory: %s\n", strerror(errno));
-    retval = 1;
-    goto cleanup;
+    return 1;
   }
 
   /* Happy path: generate and print the password(s)! */
@@ -79,8 +41,7 @@ int main(int argc, char *argv[]) {
     result = generate_password(password, password_length);
     if (result < 0) {
       fprintf(stderr, "Failed to generate password: %s\n", strerror(result));
-      retval = 1;
-      goto cleanup;
+      return 1;
     }
 
     /* Print password */
@@ -93,16 +54,59 @@ int main(int argc, char *argv[]) {
 
     if (result < 0) {
       fprintf(stderr, "Failed writing to stdout: %s\n", strerror(errno));
-      retval = 1;
-      goto cleanup;
+      return 1;
     }
   }
 
-cleanup:
-  if (password != NULL) {
-    explicit_bzero(password, password_length);
-    free(password);
-    password = NULL;
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  char *password_buf = NULL;
+  int password_length = 64;
+  int num_passwords = 1;
+  int retval = 0;
+
+  /* Check if -h or --help was specified */
+  if (should_show_usage(argc, argv)) {
+    show_usage(argc, argv);
+    return 0;
+  }
+
+  /* Parse password length argument, if provided */
+  if (argc >= 2) {
+    password_length = atoi(argv[1]);
+    if (password_length <= 0) {
+      fprintf(stderr, "Password length must be greater than zero.\n");
+      return 1;
+    }
+  }
+
+  /* Parse number of passwords argument, if provided */
+  if (argc >= 3) {
+    num_passwords = atoi(argv[2]);
+    if (num_passwords <= 0) {
+      fprintf(stderr,
+              "Number of passwords to generate must be greater than zero.\n");
+      return 1;
+    }
+  }
+
+  /* Allocate memory */
+  password_buf = calloc(1, password_length);
+  if (password_buf == NULL) {
+    fprintf(stderr, "Failed to allocate memory: %s\n", strerror(errno));
+    return 1;
+  }
+
+  /* Generate and print passwords */
+  retval = do_password_generation(password_buf, password_length, num_passwords);
+
+  /* Cleanup */
+  if (password_buf != NULL) {
+    explicit_bzero(password_buf, password_length);
+    free(password_buf);
+    password_buf = NULL;
   }
   return retval;
 }
